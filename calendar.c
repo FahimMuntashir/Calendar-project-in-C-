9b7ddd9d0e002013d6167a21b7f0ad004db1fed5
FahimMuntashir <fahim.tech.me@gmail.com>
2020-09-16 01:33:33 +0600

    working on

--- calendar.c
+++ calendar.c
@@ -9,18 +9,25 @@ int main()
     // char *days[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
 
     int days[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    int totalDays;
+    int totalDays,weekDay=0;
     for (int i = 0; i < 12; i++)
     {
-        printf("\n|------------------------%s-------------------|\n", month[i]);
+        printf("\n\n\n|--------------------%s-------------------|\n", month[i]);
         printf("\n");
 
-        printf("Sun    Mon    Tue     Wed    Thu    Fri      Sat     \n");
+        printf("   Sun   Mon   Tue    Wed   Thu   Fri   Sat \n");
 
         totalDays = days[i];
         for (int j = 1; j<=totalDays; j++)
         {
-            printf("%5d", j);
+            printf("%5d ", j);
+            weekDay++;
+
+            if (weekDay>6)
+            {
+                weekDay = 0;
+                printf("\n");
+            }
             
         }
         printf("\n");
